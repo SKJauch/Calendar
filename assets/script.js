fba55a21115f380d1f93d38def8b173c6bafef30
Stephanie Jauch <sjauch21@gmail.com>
2023-03-13 12:01:57 -0500

    link js to html

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,8 @@
 
 $(function formatDate(dateObject) {
 
+  
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
